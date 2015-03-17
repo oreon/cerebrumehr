2b3335f2c005c24ce1e58572ecdb175a0d4cb923
singhj2014 <singhjess@gmail.com>
2015-03-17 22:27:06 +0530

    made compile work

---
+++ src/test/java/com/oreon/cerebrum/web/action/admission/AdmissionActionTestBase.java
@@ -0,0 +1,46 @@
+package com.oreon.cerebrum.web.action.admission;
+
+import org.witchcraft.seam.action.BaseAction;
+
+import com.oreon.cerebrum.admission.Admission;
+
+public class AdmissionActionTestBase
+		extends
+			org.witchcraft.action.test.BaseTest<Admission> {
+
+	AdmissionAction admissionAction = new AdmissionAction();
+
+	@Override
+	public BaseAction<Admission> getAction() {
+		return admissionAction;
+	}
+
+	//@Test
+	public void testTransfer() throws Exception {
+		new ComponentTest() {
+
+			protected void testComponents() throws Exception {
+				AdmissionAction admissionAction = (AdmissionAction) org.jboss.seam.Component
+						.getInstance("admissionAction");
+
+				// assert(admissionAction.transfer()).equals("");
+			}
+
+		}.run();
+	}
+
+	//@Test
+	public void testDischarge() throws Exception {
+		new ComponentTest() {
+
+			protected void testComponents() throws Exception {
+				AdmissionAction admissionAction = (AdmissionAction) org.jboss.seam.Component
+						.getInstance("admissionAction");
+
+				// assert(admissionAction.discharge()).equals("");
+			}
+
+		}.run();
+	}
+
+}
